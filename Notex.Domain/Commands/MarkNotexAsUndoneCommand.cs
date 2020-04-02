72127ede850649a6d6c7a8ef289db53d623a9390
lucaslima10077 <lucaslima10077@gmail.com>
2020-04-01 23:23:59 -0300

    added MarkNotexAsUndoneCommand

---
+++ Notex.Domain/Commands/MarkNotexAsUndoneCommand.cs
@@ -0,0 +1,26 @@
+﻿using Flunt.Validations;
+using System;
+
+namespace Notex.Domain.Commands
+{
+    public class MarkNotexAsUndoneCommand : CommandBase
+    {
+        public MarkNotexAsUndoneCommand() { }
+
+        public MarkNotexAsUndoneCommand(Guid id, string user)
+        {
+            Id = id;
+            User = user;
+        }
+
+        public Guid Id { get; set; }
+        public string User { get; set; }
+
+        public override void Validate()
+        {
+            AddNotifications(new Contract()
+                .Requires()
+                .HasMinLen(User, 6, nameof(User), "Usuário invalido"));
+        }
+    }
+}
