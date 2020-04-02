3443cbcc09d81f30b97e47ce08852703f2e6a2ee
lucaslima10077 <lucaslima10077@gmail.com>
2020-04-01 23:18:56 -0300

    Added MarkNotexAsCommand

---
+++ Notex.Domain/Commands/MarkNotexAsDoneCommand.cs
@@ -0,0 +1,22 @@
+﻿using Flunt.Validations;
+using System;
+
+namespace Notex.Domain.Commands
+{
+    public class MarkNotexAsDoneCommand : CommandBase
+    {
+        public MarkNotexAsDoneCommand() { }
+
+        public MarkNotexAsDoneCommand(Guid id, string user)
+        {
+            Id = id;
+            User = user;
+        }
+
+        public Guid Id { get; set; }
+        public string User { get; set; }
+
+        public override void Validate() => AddNotifications(new Contract()
+                .HasMinLen(User, 6, nameof(User), "Usuário invalido"));
+    }
+}
